5b74f0949ead71385775c4a5b1563e9d4a42080c
keithdhd <keith@keithdouglas.co.uk>
2015-04-04 15:13:30 +0100

    Final version

--- js/app.js
+++ js/app.js
@@ -4,12 +4,45 @@ $(document).ready(function(){
 
 
 function activateList(){
-	$(".shopping-list li").mouseenter(function(){
+	
+	//Hover on each row and change color. Show delete button
+	$("#the-list").on('mouseenter','li', function(e){
 		$(this).children(".btn-delete").show();
-	})
+	});
 
-	$(".shopping-list li").mouseleave(function(){
+	$("#the-list").on('mouseleave','li', function(e){
 		$(this).children(".btn-delete").hide();
-	})
+	});
+
+
+	//When check is clicked, strike-through the item
+	$("#the-list").on('click','.check-me',function(){
+		$(this).parent().toggleClass("checked-off");
+	});
+	
+	//When delete button is clicked, remove item
+	$("#the-list").on('click', '.btn-delete', function() {
+		$(this).parent().remove();
+	});
+
+	//When add button is clicked or enter key pressed, add item to list
+	$(".btn").click(function(){
+		$("#warning").hide();
+		var newItem = $( ".new-item" ).val().trim();
+
+	if(newItem.length){
+		$("#the-list").append("<li><span class='check-me'>&#10004;</span>" + newItem + "<span class='btn-delete'>X</span></li>");
+	}else
+		$("#warning").show();
+	});
+
+	$(document).keyup(function(e){
+		if(e.keyCode == 13){
+			$(".btn").click();
+		};
+	});
+
+	//Drag and drop an item for sorting
+	$('#the-list').sortable({ axis: "y" });
 
 }
